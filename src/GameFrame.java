e6b26b3864865b70157a06bf7bba0da069310840
Lucky8boy <robert.nenciu@yahoo.com>
2023-09-05 16:15:02 +0300

    Update GameFrame.java
    - now songs are played on their levels

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,11 +1,11 @@
 import java.awt.Color;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 public class GameFrame {
     private JFrame game = new JFrame();
     private final int FRAME_WIDTH = 500;
     private final int FRAME_HEIGHT = 800;
-    // private Map map = new Map(FRAME_HEIGHT, null)
 
     GameFrame(int level) {
         this.game.setTitle("Level " + level);
@@ -18,9 +18,34 @@ public class GameFrame {
         this.game.setIconImage(icon.getImage());
         this.game.getContentPane().setBackground(new Color(96, 157, 243));
 
-        Map map = new Map();
+        String songPath = "";
+
+        switch(level) {
+            case 1:
+                songPath = "songs/faster_than_light.wav";
+                break;
+            case 2:
+                songPath = "songs/break_it.wav";
+                break;
+            case 3:
+                songPath = "songs/ibi_dolor.wav";
+                break;
+            case 4:
+                songPath = "songs/above_the_sea.wav";
+                break;
+            case 5:
+                songPath = "songs/blue_bird.wav";
+                break;
+            default:
+                System.out.println("There are only 5 levels! The game will close!");
+                System.exit(1);
+                break;
+        }
+
+        Map map = new Map(level, game, songPath);
         this.game.add(map);
 
         this.game.setVisible(true);
+        map = null;
     }
 }
